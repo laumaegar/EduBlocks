d6ef5916ea3e21beb7c5bf687ffc69ddeb418d68
laumaegar <40058198+laumaegar@users.noreply.github.com>
2018-11-29 23:41:37 +0100

    Update definitions.ts

--- ui/src/blocks/prueba/definitions.ts
+++ ui/src/blocks/prueba/definitions.ts
@@ -1,4 +1,14 @@
 export default function define(Blocks: Blockly.BlockDefinitions) {
- 
+ Blocks['import_prueba'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("import mylib");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(218);
+ this.setTooltip("Imports mylib ");
+ this.setHelpUrl("");
+  }
+};
  
 }
