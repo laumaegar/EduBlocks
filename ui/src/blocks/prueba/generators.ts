1b232a3163f968af977276705cad8dfc45f30115
laumaegar <40058198+laumaegar@users.noreply.github.com>
2018-11-29 23:43:25 +0100

    Update generators.ts

--- ui/src/blocks/prueba/generators.ts
+++ ui/src/blocks/prueba/generators.ts
@@ -1,4 +1,8 @@
 export default function define(Python: Blockly.BlockGenerators) {
- 
+ Python['import_prueba'] = function(block) {
+  // TODO: Assemble Python into code variable.
+  var code = 'import prueba\n';
+  return code;
+};
  
 }
